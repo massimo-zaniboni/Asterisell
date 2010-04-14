3dcee24a9f691f0171d98e112fa1e860034f5573
Massimo Zaniboni <massimo.zaniboni@profitoss.com>
2010-04-14 12:00:08 +0200

    Bug on case-insensitive VoIP accounts.

--- apps/asterisell/modules/asterisk_account/templates/_edit_footer.php
+++ apps/asterisell/modules/asterisk_account/templates/_edit_footer.php
@@ -6,9 +6,17 @@ echo insertHelp('
 
 ## Name Update
 
-If you udapte the name of a VoIP account, it will take effect only on new processed calls.
+If you update the name of a VoIP account, it will take effect only on new processed calls.
 
-In order to propagate this change also to old calls you must reset them, and force a rerate.
+In order to propagate this change also to old calls you must reset them, and force a re-rate.
+
+## VoIP Account Constraints
+
+VoIP accounts must be specified in a case-sensitive manner because PHP code manage them in case-sensitive way.
+
+But they must be also unique, using their case-insensitive version, because some SQL queries are case-insensitive.
+
+So there can not be VoIP account names like "my customer", "My customer", "MY CUSTOMER", because they are the same using a case-insensitive comparison.
 
 ');
 //}
