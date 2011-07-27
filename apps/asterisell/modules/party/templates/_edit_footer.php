eb1ee1ddb7fe3881f43b1198ae77d995b2eff44c
Massimo Zaniboni <massimo.zaniboni@profitoss.com>
2011-07-27 21:53:50 +0200

    Improved help for reseller.

--- apps/asterisell/modules/party/templates/_edit_footer.php
+++ apps/asterisell/modules/party/templates/_edit_footer.php
@@ -17,5 +17,21 @@ Directories will be created from Asterisell, if they are missing.
 
 This Asterisell installation, act like the VoIP provider of the reseller Asterisell installation.
 
+NOTE: if you enable a customer as reseller then all his previous CDR will be sent to the reseller instance of Asterisell.
+This is acceptable because usually you create a new/fresh customer/voip-account for a reseller, and you do not reuse
+a VoIP account with many CDRs in the past.
+
+In case of re-rating, the modified CDRs will be sent again to the reseller instance of Asterisell.
+So he views always the last and correct values.
+
+Ignored calls will be not sent to Asterisell reseller instance, 
+because an ignored call is not assigned to any specific account.
+A conseguence of this, it is that if you change the classification criteria for ignored calls,
+and this influence the classification of old rated CDRs already sent to a reseller,
+he will not be change them to ignored because he will not receive them with the new ignored state.
+This is acceptable because ignored call classification is a low-level, system-related
+setting, and it does not change very often, and it does not influence usually the past
+classification of CDRs.
+
 ');
 ?>
