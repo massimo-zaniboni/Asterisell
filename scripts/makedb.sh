e53c888a9df00db83c576ba2e794168a8355d2f3
Massimo Zaniboni <massimo.zaniboni@profitoss.com>
2011-12-16 13:48:28 +0100

    Improved database script.

--- scripts/makedb.sh
+++ scripts/makedb.sh
@@ -2,6 +2,7 @@
 
 #
 # Build a DB starting from Symfony Schema.
+# This script is for developers.
 #
 
 BASE=`basename $PWD`
@@ -10,7 +11,19 @@ if [ "$BASE" = "scripts" ]; then
   cd ..
   ./symfony propel-build-model
   ./symfony propel-build-sql
+
+  echo ""
+  echo "WARNING: Replace manually occurrences of "
+  echo ""
+  echo "   'Type=InnoDB;'"
+  echo ""
+  echo " with "
+  echo ""
+  echo "   'Engine=InnoDB;'"
+  echo ""
+  echo "inside 'data/sql/lib.model.schema.sql'"
+  echo "for supporting recent versions of MySQL."
+    
 else
   echo "Execute inside scripts directory."
 fi
-
