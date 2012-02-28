d7ad400dcdc181b24af11670ed88baacb2481693
Massimo Zaniboni <massimo.zaniboni@profitoss.com>
2012-02-28 12:35:05 +0100

    Minor improvements to prefix check.

--- apps/asterisell/lib/rates/phpRateWithDstChannel.class.php
+++ apps/asterisell/lib/rates/phpRateWithDstChannel.class.php
@@ -25,83 +25,102 @@ sfLoader::loadHelpers(array('I18N', 'Debug', 'Date', 'Number', 'Debug'));
 /**
  * Add a filter on Destination Channel.
  */
-abstract class PhpRateWithDstChannel extends PhpRate {
+abstract class PhpRateWithDstChannel extends PhpRate
+{
 
-  public function isForUnprocessedCDR() {
-    return false;
-  }
-
-  public $dstChannelPattern;
-  public function getDstChannelShortDescription() {
-    return PhpRateWithDstChannel::dstChannelShortDescription($this->dstChannelPattern);
-  }
-  public static function dstChannelShortDescription($chan) {
-    if (!is_null($chan) && strlen(trim($chan)) > 0) {
-      return __(' only if call channel start with ') . '"' . $chan . '" ';
-    } else {
-      return ' for all call channels';
-    }
-  }
-
-  ////////////////////////////////
-  // PHP RATE INTERFACE SUPPORT //
-  ////////////////////////////////
-
-  public function getProcessingStateType() {
-    return DestinationType::unprocessed;
-  }
-
-  public function getPriorityMethod() {
-    return "Dst Channel";
-  }
-
-  public function isApplicable($cdr, $rateInfo = null) {
-    return PhpRateWithDstChannel::isPrefixOf($this->dstChannelPattern, $cdr->getDstchannel(), true);
-  }
-
-  /**
-   * Test if $prefix is prefix of $number
-   * in case insensitive mode.
-   *
-   * @return 0 if $prefix is not a prefix of $number
-   *         the lenght of $prefix + 1 in other case
-   */
-  public static function isPrefixOf($prefix, $number, $useRegex = false) {
-
-    if (is_null($prefix)) {
-      return 1;
-      // a NULL channel is a NULL filter wich is applicable to every CDR
+    public function isForUnprocessedCDR()
+    {
+        return false;
     }
 
-    $prefix = trim($prefix);
+    public $dstChannelPattern;
 
-    $isRegex = false;
-    if ($useRegex && (strlen($prefix) > 2) && ($prefix[0] === '%' && $prefix[1] === '%')) {
-        $isRegex = true;
-        $prefix = substr($prefix, 2, strlen($prefix) - 2);
-    } 
-
-    $prefixLen = strlen($prefix);
-
-    if ($prefixLen == 0) {
-      return 1;
-      // an empty channel is a filter wich is applicable to every CDR
+    public function getDstChannelShortDescription()
+    {
+        return PhpRateWithDstChannel::dstChannelShortDescription($this->dstChannelPattern);
     }
 
-    if ($isRegex) {
-        $matches = false;
-        if (preg_match($prefix, $number, $matches) > 0) {
-	    return strlen($matches[0]) + 1;
+    public static function dstChannelShortDescription($chan)
+    {
+        if (!is_null($chan) && strlen(trim($chan)) > 0) {
+            return __(' only if call channel start with ') . '"' . $chan . '" ';
         } else {
-            return 0;
-	}
-    } else {
-      if (substr_compare($prefix, $number, 0, $prefixLen, TRUE) == 0) {
-        return $prefixLen + 1;
-      } else {
-        return 0;
-      }
+            return ' for all call channels';
+        }
     }
-  }
+
+    ////////////////////////////////
+    // PHP RATE INTERFACE SUPPORT //
+    ////////////////////////////////
+
+    public function getProcessingStateType()
+    {
+        return DestinationType::unprocessed;
+    }
+
+    public function getPriorityMethod()
+    {
+        return "Dst Channel";
+    }
+
+    public function isApplicable($cdr, $rateInfo = null)
+    {
+        return PhpRateWithDstChannel::isPrefixOf($this->dstChannelPattern, $cdr->getDstchannel(), true);
+    }
+
+    /**
+     * Test if $prefix is prefix of $number
+     * in case insensitive mode.
+     *
+     * @return int 0 if $prefix is not a prefix of $number
+     *         the lenght of $prefix + 1 in other case
+     */
+    public static function isPrefixOf($prefix, $number, $useRegex = false)
+    {
+
+        if (is_null($prefix)) {
+            return 1;
+            // a NULL channel is a NULL filter wich is applicable to every CDR
+        }
+
+        $prefix = trim($prefix);
+
+        $isRegex = false;
+        if ($useRegex && (strlen($prefix) > 2) && ($prefix[0] === '%' && $prefix[1] === '%')) {
+            $isRegex = true;
+            $prefix = substr($prefix, 2, strlen($prefix) - 2);
+        }
+
+        $prefixLen = strlen($prefix);
+
+        if ($prefixLen == 0) {
+            return 1;
+            // an empty channel is a filter wich is applicable to every CDR
+        }
+
+        if ($isRegex) {
+            $matches = false;
+            if (preg_match($prefix, $number, $matches) > 0) {
+                return strlen($matches[0]) + 1;
+            } else {
+                return 0;
+            }
+        } else {
+
+            $numberLen = strlen($number);
+
+            if ($prefixLen > $numberLen) {
+                return 0;
+            }
+
+            $numberPrefix = substr($number, 0, $prefixLen);
+
+            if (substr_compare($prefix, $numberPrefix, 0, $prefixLen, TRUE) == 0) {
+                return $prefixLen + 1;
+            } else {
+                return 0;
+            }
+        }
+    }
 }
 ?>
